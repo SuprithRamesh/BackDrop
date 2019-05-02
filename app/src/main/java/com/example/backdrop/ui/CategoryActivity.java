76fa874c924fce277372774e07512b1b8ba3fb42
Suprith <44401046+SuprithRamesh@users.noreply.github.com>
2019-05-02 01:31:38 +0100

    Added different activities

---
+++ app/src/main/java/com/example/backdrop/ui/CategoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.backdrop.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.backdrop.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
